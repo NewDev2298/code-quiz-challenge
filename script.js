47180f469b3029b3cec6d0e3f61b043583f97456
Terrance Smith <terrancesmith@Laquishas-Air.lan>
2022-12-05 21:08:41 -0500

    Added questions and event listener

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ var nextEl = document.querySelector('#next');
 var cursor = 0;
 
 var questions = [
-  "How much wood could a woodchuck chuck?",
-  "Did you have your break today?",
-  "Do you like ice cream?",
-  "What's your favorite pizza topping?"
+  "Who was the 35th President of the United States?",
+  "Dwyane Wade is most known for his association with which basketball club?",
+  "Robyn Fenty is the real name of what popular singer?",
+  "Meryl Streep is _ time Oscar winner.",
+  "In what city will the 2022 World Cup be located?"
 ];
 
 var displayQuestion = function () {
